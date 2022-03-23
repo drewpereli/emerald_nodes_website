1097c2675bf51a65ff02cb88753c6a2b7f9d598b
Drew Pereli <drewpereli@gmail.com>
2022-03-22 21:56:43 -0700

    Add pb-4 to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const imagesPath = '../images'; // Relative to ./src/styles/app.css
 
 module.exports = {
   content: ['./src/**/*.pug'],
+  safelist: ['pb-4'],
   theme: {
     extend: {
       colors: {
