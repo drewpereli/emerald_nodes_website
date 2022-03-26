342f4b581d4adc695ca02884a83aedbcb9a65c1c
Drew Pereli <drewpereli@gmail.com>
2022-03-25 17:38:40 -0700

    Properly show/hide links in get started section

--- src/scripts/launch-transitions.ts
+++ src/scripts/launch-transitions.ts
@@ -1,6 +1,10 @@
 export const NODES_APP_LAUNCH_TIME = 'March 29 2022 17:00:00 GMT-0500';
 const SWAP_APP_LAUNCH_TIME = 'March 26 2022 10:00:00 PDT';
 
+// const soon = new Date(Date.now() + 3000).toString();
+// export const NODES_APP_LAUNCH_TIME = soon;
+// const SWAP_APP_LAUNCH_TIME = soon;
+
 export function getTimeRemaining(endTime: string) {
   return Date.parse(endTime) - Date.parse(new Date().toString());
 }
